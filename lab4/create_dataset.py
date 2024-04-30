b9331d6d6a23744de4fe588e29125277c9c60dfd
ANGorbachev <a.n.gorba4ev@gmail.com>
2024-04-30 23:53:35 +0300

    init dvc

--- lab4/create_dataset.py
+++ lab4/create_dataset.py
@@ -6,4 +6,4 @@ titanic_train = titanic_train[['PassengerId', 'Survived', 'Name', 'Pclass', 'Sex
 titanic_test = titanic_test[['PassengerId', 'Name', 'Pclass', 'Sex', 'Age']]
 
 pd.DataFrame(titanic_train).to_csv("./datasets/titanic_train.csv", index=False)
-pd.DataFrame(titanic_test).to_csv("./datasets/tdvcitanic_test.csv", index=False)
+pd.DataFrame(titanic_test).to_csv("./datasets/titanic_test.csv", index=False)
