7b9f6d71a82cd9e4f35d7241bb741a362e971c4b
ANGorbachev <a.n.gorba4ev@gmail.com>
2024-05-01 00:02:42 +0300

    dvc 3rd version of dataset (updated)

--- lab4/data_modification2.py
+++ lab4/data_modification2.py
@@ -8,5 +8,9 @@ encoder = OneHotEncoder()
 enc_df = pd.DataFrame(encoder.fit_transform(titanic_train[['Sex']]).toarray())
 titanic_train = pd.concat([titanic_train, enc_df], axis=1)
 
+encoder = OneHotEncoder()
+enc_df = pd.DataFrame(encoder.fit_transform(titanic_test[['Sex']]).toarray())
+titanic_test = pd.concat([titanic_test, enc_df], axis=1)
+
 pd.DataFrame(titanic_train).to_csv("./datasets/titanic_train.csv", index=False)
 pd.DataFrame(titanic_test).to_csv("./datasets/titanic_test.csv", index=False)
