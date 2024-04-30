99a4dbe0ce8696d7a1f7abb757a91f7e80a07a9c
ANGorbachev <a.n.gorba4ev@gmail.com>
2024-04-30 22:33:26 +0300

    Add first version

---
+++ lab4/create_dataset.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from catboost.datasets import titanic
+titanic_train, titanic_test = titanic()
+
+titanic_train = titanic_train[['PassengerId', 'Survived', 'Name', 'Pclass', 'Sex', 'Age']]
+titanic_test = titanic_test[['PassengerId', 'Name', 'Pclass', 'Sex', 'Age']]
+
+pd.DataFrame(titanic_train).to_csv("./datasets/titanic_train.csv", index=False)
+pd.DataFrame(titanic_test).to_csv("./datasets/titanic_test.csv", index=False)
